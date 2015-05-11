3371993e3b07480ea1ff88c35c54d5c2e272fcff
Dmitry Ponyatov <dponyatov@gmail.com>
2015-05-11 19:31:07 +0400

    + Makefile deps include
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ mk.mk
@@ -0,0 +1,9 @@
+TEX += ../texheader/header.tex 
+TEX += ../texheader/title.tex
+TEX += ../texheader/colors.tex
+TEX += ../texheader/comp.tex
+TEX += ../texheader/translate.tex
+TEX += ../texheader/lstmk.sty 
+TEX += ../texheader/lstrc.sty 
+TEX += ../texheader/lstsyslinux.sty 
+TEX += ../texheader/lstobjdump.sty
