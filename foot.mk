03c4d9d3e73127fd406599870291bdd4c7c8882c
Dmitry Ponyatov <dponyatov@gmail.com>
2016-09-20 13:00:32 +0400

    * mk /foot
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- foot.mk
+++ foot.mk
@@ -3,7 +3,7 @@ LATEX = pdflatex -halt-on-error
 
 .PHONY: pdf
 pdf: $(MODULE).pdf
-$(MODULE).pdf: $(TX) $(FIG) $(SRC)
+$(MODULE).pdf: $(TEX) $(FIG) $(SRC)
 	$(LATEX) $(MODULE) && makeindex $(MODULE) && $(LATEX) $(MODULE)
 
 tmp/%.pdf: fig/%.dot
